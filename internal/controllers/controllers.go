fec6568cde18e37ac0afa723e8ed94593779ac0b
v_amerk <devamerkh@gmail.com>
2024-12-03 02:22:32 +0500

    refresh token + fix access token

--- internal/controllers/controllers.go
+++ internal/controllers/controllers.go
@@ -3,10 +3,10 @@ package controllers
 import (
 	"authService/internal/store"
 	"authService/pkg/generator"
-	"encoding/base64"
 	"encoding/json"
 	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
+	"log"
 	"net/http"
 	"time"
 )
@@ -24,7 +24,7 @@ func AccessMethod(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	accessToken, err := generateAccessToken(body.GuId, r.RemoteAddr, time.Minute*5) // Ну вроде по заданию надо guid и ip
+	accessToken, err := generateAccessToken(body.GuId, r.RemoteAddr, time.Minute*5)
 	if err != nil {
 		http.Error(w, "Failed to generate access token", http.StatusInternalServerError)
 		return
@@ -53,7 +53,53 @@ func AccessMethod(w http.ResponseWriter, r *http.Request) {
 }
 
 func RefreshMethod(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		RefreshToken string `json:"refresh_token"`
+	}
 
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&body)
+	if err != nil {
+		http.Error(w, "Invalid JSON body", http.StatusBadRequest)
+		return
+	}
+
+	user, err := store.FindUserByRefreshToken(body.RefreshToken)
+	if err != nil {
+		http.Error(w, "Failed to find user", http.StatusNotFound)
+		return
+	}
+
+	if user.Ip != r.RemoteAddr {
+		// Отправляем на почту уведомление, что вйпишник другой
+	}
+
+	newAccessToken, err := generateAccessToken(user.Guid, r.RemoteAddr, time.Minute*5)
+	if err != nil {
+		http.Error(w, "Failed to generate access token", http.StatusInternalServerError)
+		return
+	}
+
+	newRefreshToken, newHashedRefreshToken, err := generateRefreshToken()
+	if err != nil {
+		http.Error(w, "Failed to generate refresh token", http.StatusInternalServerError)
+		return
+	}
+
+	err = store.UpdateRefreshToken(user.Guid, newHashedRefreshToken, r.RemoteAddr)
+	if err != nil {
+		http.Error(w, "Failed to update refresh token", http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]string{
+		"access_token":  newAccessToken,
+		"refresh_token": newRefreshToken,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
 }
 
 func generateAccessToken(guid, ip string, expiration time.Duration) (string, error) {
@@ -77,8 +123,8 @@ func generateRefreshToken() (string, string, error) {
 	if err != nil {
 		return "", "", err
 	}
-
-	encodedToken := base64.StdEncoding.EncodeToString([]byte(refreshToken))
-
-	return encodedToken, string(hashedToken), nil
+	//
+	//encodedToken := base64.StdEncoding.EncodeToString([]byte(refreshToken))
+	log.Println(refreshToken, hashedToken)
+	return refreshToken, string(hashedToken), nil
 }
--- internal/store/user.go
+++ internal/store/user.go
@@ -3,6 +3,7 @@ package store
 import (
 	"authService/internal/models"
 	"errors"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func StoreRefreshToken(guid, hashedToken, ip, email string) error {
@@ -26,9 +27,6 @@ func StoreRefreshToken(guid, hashedToken, ip, email string) error {
 		if ip != "" {
 			user.Ip = ip
 		}
-		if ip != user.Ip {
-			// Отправляем на почту уведомление, что вйпишник другой
-		}
 
 		res = Db.Save(&user)
 		if res.Error != nil {
@@ -36,4 +34,41 @@ func StoreRefreshToken(guid, hashedToken, ip, email string) error {
 		}
 	}
 	return nil
+}
+
+func FindUserByRefreshToken(refreshToken string) (*models.User, error) {
+	var users []models.User
+
+	res := Db.Find(&users)
+	if res.Error != nil {
+		return nil, errors.New("Failed to retrieve users")
+	}
+
+	for _, user := range users {
+		err := bcrypt.CompareHashAndPassword([]byte(user.RefreshToken), []byte(refreshToken))
+		if err == nil {
+			return &user, nil
+		}
+	}
+
+	return nil, errors.New("Failed to find user by refresh token")
+}
+
+func UpdateRefreshToken(guid, newHashedToken, ip string) error {
+	var user models.User
+
+	res := Db.First(&user, "guid = ?", guid)
+	if res.Error != nil {
+		return errors.New("Failed to find user by refresh token")
+	} else {
+		user.RefreshToken = newHashedToken
+		if ip != "" {
+			user.Ip = ip
+		}
+		res = Db.Save(&user)
+		if res.Error != nil {
+			return errors.New("Failed to update refresh token")
+		}
+		return nil
+	}
 }
