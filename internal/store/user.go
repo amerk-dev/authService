f8551dde660dec18debd38c8a7f39d3b87e7f73d
v_amerk <devamerkh@gmail.com>
2024-12-02 20:50:04 +0500

    replace token

--- internal/store/user.go
+++ internal/store/user.go
@@ -6,15 +6,21 @@ import (
 )
 
 func StoreRefreshToken(guid, hashedToken string) error {
-	user := Db.First(models.User{}, "guid = ?", guid)
-	if user == nil {
-		err := user.Create(models.User{Guid: guid, RefreshToken: hashedToken})
-		if err != nil {
+	var user models.User
+
+	res := Db.First(&user, "guid = ?", guid)
+	if res.Error != nil {
+		user = models.User{Guid: guid, RefreshToken: hashedToken}
+		res = Db.Create(&user)
+		if res.Error != nil {
 			return errors.New("Failed to store refresh token")
 		}
 	} else {
-		// Update юзера
+		user.RefreshToken = hashedToken
+		res = Db.Save(&user)
+		if res.Error != nil {
+			return errors.New("Failed to update refresh token")
+		}
 	}
-	Db.Save(&user)
 	return nil
 }
